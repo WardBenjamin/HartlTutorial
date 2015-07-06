40a96546f489e720f10426584708bf90a7ee493d
Benjamin Ward <ward.programm3r@gmail.com>
2015-07-06 11:35:39 -0400

    Complete ch.8 (user auth/cookies)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Programm3r's Domain!"
       redirect_to @user
     else
