d4ba6718217cbce7a0d0236bbb657816104d7662
Benjamin Ward <ward.programm3r@gmail.com>
2015-07-09 12:14:07 -0400

    Microposts now resize images (max 400x400)

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,10 +1,9 @@
 # encoding: utf-8
 
 class PictureUploader < CarrierWave::Uploader::Base
-
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -25,6 +24,9 @@ class PictureUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
+  
+  process resize_to_limit: [400, 400]
+  
   # process :scale => [200, 300]
   #
   # def scale(width, height)
